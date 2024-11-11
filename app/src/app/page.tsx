12b2b8b269411806fe4e23e3d237200ea810b0b7
Ramesha javed <165018589+Ramesha-sheikh@users.noreply.github.com>
2024-11-12 02:39:50 +0500

    first commit

---
+++ app/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Helloworld from '@/components/hello world'
+
+export default function Home() {
+  return (
+ <Helloworld/>
+  );
+}
---
+++ app/src/components/hello world.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Helloworld = () => {
+  return (
+    <div>
+      <h1 className='text-center bg-fuchsia-600 py-20'>Hello World</h1>
+    </div>
+  )
+}
+
+export default Helloworld
